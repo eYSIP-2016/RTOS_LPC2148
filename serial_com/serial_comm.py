b858e7ac11e221c073bd10299a45ac4ba8571854
sumakar <sumakarkvs@gmail.com>
2016-07-16 23:32:08 +0530

    Data being stored with time stamp

--- serial_com/serial_comm.py
+++ serial_com/serial_comm.py
@@ -1,21 +1,30 @@
 import serial
+import time
+import datetime
 
-ser = serial.Serial(port='COM6',timeout=None, baudrate= 9600)
+ser = serial.Serial(port='COM6',timeout=5, baudrate= 9600)
 flag = ser.isOpen()
 saveFile = open('t2.txt','w')
 count=0
+time_stamp = time.time()
+date_stamp = datetime.datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+saveFile.write(str(date_stamp) + "\n")
 
 while (count<9):
- data = ser.read(16) ;
+ data = ser.read(17) ;
  co=0
-
+ time_stamp = time.time()
+ date_stamp = datetime.datetime.fromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+ saveFile.write(str(time_stamp) + ":" + "\t")
  while (co<16):
 
    print(data[co])
    saveFile.write(str(data[co]))
+   saveFile.write(",")
    co=co+1;
- #print(data)
+ 
  count=count+1;
  
- #saveFile.write("\n")
+ saveFile.write("\n")
+ 
 saveFile.close()
