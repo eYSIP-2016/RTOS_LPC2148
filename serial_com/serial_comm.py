55d4ab9916f40480c280b3d25f8eab9ce1cc5bee
sumakar <sumakarkvs@gmail.com>
2016-07-16 20:23:55 +0530

    data available as numbers

--- serial_com/serial_comm.py
+++ serial_com/serial_comm.py
@@ -1,7 +1,21 @@
 import serial
+
 ser = serial.Serial(port='COM6',timeout=None, baudrate= 9600)
 flag = ser.isOpen()
+saveFile = open('t2.txt','w')
+count=0
 
-while 1:
- data = ser.read(14) ;
- print(data)
+while (count<9):
+ data = ser.read(16) ;
+ co=0
+
+ while (co<16):
+
+   print(data[co])
+   saveFile.write(str(data[co]))
+   co=co+1;
+ #print(data)
+ count=count+1;
+ 
+ #saveFile.write("\n")
+saveFile.close()
