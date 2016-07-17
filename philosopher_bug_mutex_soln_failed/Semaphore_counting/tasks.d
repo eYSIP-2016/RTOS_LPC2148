747d358e7ec3a4d2e1540417914304d91bd87eb8
sumakar <sumakarkvs@gmail.com>
2016-07-17 23:48:49 +0530

    Failed Mutex dining philo
    Dining philosopher string problem tried to solve using mutex

---
+++ philosopher_bug_mutex_soln_failed/Semaphore_counting/tasks.d
@@ -0,0 +1,17 @@
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\tasks.c
+tasks.o: C:\Keil\ARM\RV31\INC\stdlib.h
+tasks.o: C:\Keil\ARM\RV31\INC\string.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\FreeRTOS.h
+tasks.o: C:\Keil\ARM\RV31\INC\stddef.h
+tasks.o: C:\Keil\ARM\RV31\INC\stdint.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\FreeRTOSConfig.h
+tasks.o: C:\Keil\ARM\INC\Philips\lpc21xx.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\projdefs.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\portable.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\deprecated_definitions.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\..\..\Source\portable\RVDS\ARM7_LPC21xx\portmacro.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\mpu_wrappers.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\task.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\list.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\timers.h
+tasks.o: S:\FreeRTOSv9.0.0\FreeRTOS\Source\include\StackMacros.h
